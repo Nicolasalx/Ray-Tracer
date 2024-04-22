8bc173f6b7a3d6337c2038bde9bd4cc54bce7430
Guillaume <guillaume.dubuis@epitech.eu>
2024-04-22 16:35:15 +0200

    add: lib-config++ and start parsing of simple_scene file

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,7 @@
 #include "Dielectric.hpp"
 #include "BoundingNode.hpp"
 #include <thread>
+#include <libconfig.h++>
 
 std::size_t Rt::Interface::image_pixel_i = 0;
 sf::Uint8 *Rt::Interface::image_pixel = nullptr;
@@ -34,6 +35,63 @@ std::atomic_bool Rt::Raytracer::end_rendering(false);
 
 std::size_t Rt::Interface::image_size_x = 480;
 std::size_t Rt::Interface::image_size_y = 270;
+
+void loadCfg(const std::string& configFile, Rt::ObjectList& world, Rt::Camera& cam)
+{
+    libconfig::Config cfg;
+    try {
+        cfg.readFile(configFile.c_str());
+    } catch(const libconfig::FileIOException& fioex) {
+        std::cerr << "Erreur lors de la lecture du fichier de configuration." << std::endl;
+        return;
+    } catch(const libconfig::ParseException& pex) {
+        std::cerr << "Erreur de syntaxe dans le fichier de configuration à la ligne " << pex.getLine() << ": " << pex.getError() << std::endl;
+        return;
+    }
+
+    const libconfig::Setting& cameraSettings = cfg.lookup("camera");
+    const libconfig::Setting& primitivesSettings = cfg.lookup("primitives");
+    const libconfig::Setting& res = cameraSettings["resolution"];
+    const libconfig::Setting& pos = cameraSettings["position"];
+    const libconfig::Setting& rot = cameraSettings["rotation"];
+    int resolution_width = res["width"];
+    int resolution_height = res["height"];
+    int position_x = pos["x"];
+    int position_y = pos["y"];
+    int position_z = pos["z"];
+    int rotation_x = rot["x"];
+    int rotation_y = rot["y"];
+    int rotation_z = rot["z"];
+    float field_of_view;
+    cameraSettings.lookupValue("fieldOfView", field_of_view);
+
+    const libconfig::Setting& spheresSettings = primitivesSettings["spheres"];
+    for (int i = 0; i < spheresSettings.getLength(); ++i) {
+        const libconfig::Setting& sphere = spheresSettings[i];
+        int x, y, z, r, color_r, color_g, color_b;
+        sphere.lookupValue("x", x);
+        sphere.lookupValue("y", y);
+        sphere.lookupValue("z", z);
+        sphere.lookupValue("r", r);
+        const libconfig::Setting& color = sphere["color"];
+        color.lookupValue("r", color_r);
+        color.lookupValue("g", color_g);
+        color.lookupValue("b", color_b);
+    }
+
+    const libconfig::Setting& planesSettings = primitivesSettings["planes"];
+    for (int i = 0; i < planesSettings.getLength(); ++i) {
+        const libconfig::Setting& plane = planesSettings[i];
+        std::string axis;
+        int position, color_r, color_g, color_b;
+        plane.lookupValue("axis", axis);
+        plane.lookupValue("position", position);
+        const libconfig::Setting& color = plane["color"];
+        color.lookupValue("r", color_r);
+        color.lookupValue("g", color_g);
+        color.lookupValue("b", color_b);
+    }
+}
 
 void Rt::Raytracer::launchRendering()
 {
@@ -67,13 +125,15 @@ void Rt::Raytracer::launchRendering()
     cam.render(world);
 }
 
-int main(int argc, const char *argv[])
-{
+int main(int argc, const char *argv[]) {
     try {
         Rt::Interface interface(480, 270);
+        std::string configFile = "simple_scene";
+        Rt::ObjectList world;
+        Rt::Camera cam;
+        loadCfg(configFile, world, cam);
 
         std::thread render_thread(Rt::Raytracer::launchRendering);
-
         interface.loop();
         Rt::Raytracer::end_rendering = true;
         render_thread.join();
@@ -83,3 +143,4 @@ int main(int argc, const char *argv[])
     }
     return 0;
 }
+
