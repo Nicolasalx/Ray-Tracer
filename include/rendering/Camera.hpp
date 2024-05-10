12bab03a0fc61124a80e96a1f2a5ae597e49c56f
Guillaume <guillaume.dubuis@epitech.eu>
2024-05-10 17:26:37 +0200

    mod: include camera useless variable

--- include/rendering/Camera.hpp
+++ include/rendering/Camera.hpp
@@ -25,27 +25,25 @@ namespace Rt
         int image_width = 1920;
         int image_height = 1080;
         double fov = 90;
-        int samples_per_pixel = 100;   // Count of random samples for each pixel
-        int max_depth = 10;   // Maximum number of ray bounces into scene
+        int samples_per_pixel = 100;
+        int max_depth = 10;
         std::size_t nb_thread = 16;
         Math::Point3D lookfrom = Math::Point3D(0,0,0);
         Math::Point3D lookat = Math::Point3D(0,0,-1);
         Math::Vector3D vup = Math::Vector3D(0,1,0);
-        Math::Color01 background;               // Scene background color
+        Math::Color01 background;
 
-//        Math::Point3D position = Math::Point3D(0, 0, 0);   // Camera position
-//        Math::Vector3D rotation = Math::Vector3D(0, 0, 0); // Camera rotation
 
         void render(const Rt::IObject &world);
 
     private:
 
-        Math::Point3D center;         // Camera center
-        Math::Point3D pixel00_loc;    // Location of pixel 0, 0
-        Math::Vector3D   pixel_delta_u;  // Offset to pixel to the right
-        Math::Vector3D   pixel_delta_v;  // Offset to pixel below
-        double pixel_samples_scale;  // Color scale factor for a sum of pixel samples
-        Math::Vector3D   u, v, w;              // Camera frame basis vectors
+        Math::Point3D center;
+        Math::Point3D pixel00_loc;
+        Math::Vector3D pixel_delta_u;
+        Math::Vector3D pixel_delta_v;
+        double pixel_samples_scale;
+        Math::Vector3D u, v, w;
 
         void init();
         Rt::Ray get_ray(int i, int j) const;
