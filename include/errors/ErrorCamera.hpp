b848eea4e45b51e670c5646843637491344edc86
Guillaume <guillaume.dubuis@epitech.eu>
2024-05-12 19:11:13 +0200

    add: class errors

---
+++ include/errors/ErrorCamera.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** B-OOP-400-PAR-4-1-raytracer-thibaud.cathala
+** File description:
+** ErrorCamera
+*/
+
+#ifndef ERRORCAMERA_HPP_
+#define ERRORCAMERA_HPP_
+
+#include <iostream>
+
+class ErrorCamera {
+    public:
+        ErrorCamera(std::ostream &, const std::string &) throw();
+    protected:
+    private:
+};
+
+#endif /* !ERRORCAMERA_HPP_ */
---
+++ src/errors/errorObj.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** B-OOP-400-PAR-4-1-raytracer-thibaud.cathala
+** File description:
+** errorObj
+*/
+
+#include "ErrorObj.hpp"
+
+ErrorObj::ErrorObj(std::ostream &os, const std::string &message) throw()
+{
+    os << message << std::endl;
+}
